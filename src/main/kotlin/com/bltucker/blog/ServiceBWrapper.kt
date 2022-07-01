b4c8a799915ce9f8b2a4a55b4680991ebd5c4e47
Brett <bretttucker@gmail.com>
2022-07-01 13:04:17 -0600

    Add circuit breaker example

---
+++ src/main/kotlin/com/bltucker/blog/ServiceBWrapper.kt
@@ -0,0 +1,18 @@
+package com.bltucker.blog
+
+import kotlinx.coroutines.delay
+import kotlin.random.Random
+
+class ServiceBWrapper {
+
+
+    suspend fun unreliableRemoteCall() : String{
+        delay(100)
+        return if(Random.nextInt(100) < 33){
+            throw Exception("Call Failed")
+        } else {
+            "data"
+        }
+    }
+
+}
--- src/main/kotlin/com/bltucker/blog/plugins/Routing.kt
+++ src/main/kotlin/com/bltucker/blog/plugins/Routing.kt
@@ -1,6 +1,11 @@
 package com.bltucker.blog.plugins
 
 import com.bltucker.blog.DataRepository
+import com.bltucker.blog.ServiceBWrapper
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry
+import io.github.resilience4j.kotlin.circuitbreaker.CircuitBreakerConfig
+import io.github.resilience4j.kotlin.circuitbreaker.executeSuspendFunction
 import io.github.resilience4j.kotlin.ratelimiter.RateLimiterConfig
 import io.github.resilience4j.kotlin.ratelimiter.executeSuspendFunction
 import io.github.resilience4j.ratelimiter.RateLimiterRegistry
@@ -13,17 +18,32 @@ import java.time.Duration
 
 fun Application.configureRouting() {
 
-    val config = RateLimiterConfig {
+    val rateLimiterConfig = RateLimiterConfig {
         timeoutDuration(Duration.ofSeconds(5))
         limitRefreshPeriod(Duration.ofMinutes(1))
         limitForPeriod(10)
     }
 
-    val rateLimiterRegistry = RateLimiterRegistry.of(config)
+    val rateLimiterRegistry = RateLimiterRegistry.of(rateLimiterConfig)
 
     val importantResourceRateLimiter = rateLimiterRegistry.rateLimiter("importantResource")
 
     val repository = DataRepository()
+
+    val circuitBreakerConfig = CircuitBreakerConfig {
+        failureRateThreshold(25F)
+        permittedNumberOfCallsInHalfOpenState(5)
+        slidingWindowType(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+        slidingWindowSize(5)
+        minimumNumberOfCalls(5)
+    }
+
+    val circuitBreakerRegistry = CircuitBreakerRegistry.of(circuitBreakerConfig)
+
+    val serviceBCircuitBreaker = circuitBreakerRegistry.circuitBreaker("ServiceB")
+
+    val serviceBWrapper = ServiceBWrapper()
+
 
     routing {
         get("/someImportantResource") {
@@ -36,6 +56,18 @@ fun Application.configureRouting() {
                 call.respond(mapOf("data" to data))
             } catch(ex: RequestNotPermitted){
                 call.respond(HttpStatusCode.TooManyRequests)
+            }
+        }
+
+        get("/someUnreliableResource"){
+            try{
+                val data = serviceBCircuitBreaker.executeSuspendFunction {
+                    serviceBWrapper.unreliableRemoteCall()
+                }
+
+                call.respond(mapOf("data" to data))
+            } catch(ex: CallNotPermittedException){
+                call.respond(HttpStatusCode.ServiceUnavailable)
             }
         }
     }
